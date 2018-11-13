3d57199621aad4d5a60497d61cec4e26dbe86015
Gessica <gessica@dymaxionlabs.com>
2018-11-13 13:08:26 -0300

    Maps

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MapGL from 'react-map-gl';
 
 class App extends Component {
+
+  state = {
+    viewport: {
+      width: "100vw",
+      height: "100vh",
+      latitude: -34.609032,
+      longitude: -58.373219,
+      zoom: 12,
+      mapStyle: 'mapbox://styles/mapbox/satellite-v9'
+    }
+
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <MapGL
+        {...this.state.viewport}
+        onViewportChange={(viewport) => this.setState({ viewport })}
+        mapboxApiAccessToken="pk.eyJ1IjoiZ2Vzc2ljYTExMTIiLCJhIjoiY2pvZnYwYmV0MDhrYjNxanRpc2E3enhydiJ9.fawTIAVKzqpOE41wkVw1Zw"
+      />
     );
   }
 }

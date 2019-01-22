f5b379001e99c4603b72cd3710a5720f967ea733
Damián Silvani <munshkr@gmail.com>
2019-01-22 17:35:33 -0300

    Better components and styles for ConfirmationPortal

--- components/ConfirmationPortal.js
+++ components/ConfirmationPortal.js
@@ -1,21 +1,26 @@
 import React from "react";
+import { Segment, Header } from "semantic-ui-react";
 import ModalContactForm from "./ModalContactForm";
 import { allLayers } from "./LayerSelector";
 
-const style = {
-  position: "absolute",
-  right: 40,
-  top: 80,
-  background: "#fff",
-  padding: "12px 24px",
-  width: "300px",
-  zIndex: 1000,
+const AreaSection = ({ value }) => (
+  <Header as="h4">
+    {Math.round(value)} km²
+    <Header.Subheader>tamaño de superficie</Header.Subheader>
+  </Header>
+);
 
-};
+const LayersSection = ({ children }) => (
+  <Header as="h4">
+    {children}
+    <Header.Subheader>capas seleccionadas</Header.Subheader>
+  </Header>
+);
 
 class ConfirmationPortal extends React.Component {
   _layersSentence() {
     const selectedLayers = this.props.selectedLayers || [];
+
     return allLayers
       .filter(layer => selectedLayers.includes(layer.key))
       .map(layer => layer.text)
@@ -23,22 +28,31 @@ class ConfirmationPortal extends React.Component {
   }
 
   render() {
-
     const { area } = this.props;
+    const layers = this._layersSentence();
 
     return (
-      <div style={style}>
-        <div className="separator">
-          <p className="Line-control">
-            <b>{(Math.round(area))}</b> km²{" "}
-          </p>
-          <p className="Control Line-control">tamaño de superficie</p>
-        </div>
-        <div className="separator">
-          <p className="Line-control">{this._layersSentence()}</p>
-          <p className="Control Line-control">capas seleccionadas</p>
-        </div>
+      <div className="confirmation-portal">
+        <Segment>
+          <AreaSection value={area} />
+          {layers && <LayersSection>{layers}</LayersSection>}
+        </Segment>
         <ModalContactForm />
+
+        <style jsx>{`
+          .confirmation-portal {
+            background-color: #fff;
+            padding: 10px;
+            box-shadow: 0px 2px 4px 0px rgba(34, 36, 38, 0.12),
+              0px 2px 10px 0px rgba(34, 36, 38, 0.15);
+            border-radius: 4px;
+            position: absolute;
+            right: 80px;
+            top: 50px;
+            width: 300px;
+            z-index: 1000;
+          }
+        `}</style>
       </div>
     );
   }

05cfcea1bb3f2343cb5d1457590a683b3d876367
Damián Silvani <munshkr@gmail.com>
2019-03-10 00:33:22 -0300

    Show progress when uploading files. Allow cancelling

--- components/ImageUploadDialog.js
+++ components/ImageUploadDialog.js
@@ -5,8 +5,12 @@ import { withStyles } from "@material-ui/core/styles";
 import { buildApiUrl } from "../utils/api";
 import DropzoneDialog from "./upload/DropzoneDialog";
 import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogActions from "@material-ui/core/DialogActions";
+import LinearProgress from "@material-ui/core/LinearProgress";
 import CloudUploadIcon from "@material-ui/icons/CloudUpload";
-import { Typography } from "@material-ui/core";
 import axios from "axios";
 
 const styles = theme => ({
@@ -24,32 +28,73 @@ const styles = theme => ({
   }
 });
 
+class UploadProgressDialog extends Component {
+  handleClose = event => {
+    const { onClose } = this.props;
+    if (onClose) onClose(event);
+  };
+
+  render() {
+    const { open, progress } = this.props;
+
+    return (
+      <Dialog
+        open={open}
+        onClose={this.handleClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogTitle id="form-dialog-title">Uploading...</DialogTitle>
+        <DialogContent>
+          <LinearProgress variant="determinate" value={progress} />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={this.handleClose} color="primary">
+            Cancel
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
+}
+
+UploadProgressDialog = withStyles(styles)(UploadProgressDialog);
+
 class ImageUploadDialog extends Component {
   state = {
     open: false,
-    files: []
+    uploading: false,
+    uploadProgress: 0
   };
 
   handleClose = () => {
-    this.setState({ open: false });
+    // FIXME if uploading, ask user if she wants to cancel...
+    this.setState({ open: false, uploading: false });
   };
 
   handleSave = files => {
+    if (files.length == 0) return;
+
+    this.setState({ uploading: true, uploadProgress: 0 });
+
+    let count = 0;
     files.forEach(file => {
       axios
-        .post(
-          buildApiUrl(`/images/upload/${file.name}`),
-          { data: file },
-          { headers: { Authorization: this.props.token } }
-        )
-        .then(data => {
-          console.log(data);
+        .post(buildApiUrl(`/images/upload/${file.name}`), file, {
+          headers: { Authorization: this.props.token }
+        })
+        .then(() => {})
+        .catch(err => {
+          console.error(err);
+        })
+        .then(() => {
+          count += 1;
+          this.setState({ uploadProgress: (count / files.length) * 100 });
+          if (count === files.length) {
+            this.setState({ open: false, uploading: false });
+          }
         });
-    });
-    // Saving files to state for further use and closing Modal.
-    this.setState({
-      files: files,
-      open: false
+
+      if (!this.state.uploading) return;
     });
   };
 
@@ -59,9 +104,13 @@ class ImageUploadDialog extends Component {
     });
   };
 
+  handleUploadCancel = () => {
+    this.setState({ uploading: false });
+  };
+
   render() {
     const { classes } = this.props;
-    const { open } = this.state;
+    const { open, uploading, uploadProgress } = this.state;
 
     return (
       <div>
@@ -77,6 +126,11 @@ class ImageUploadDialog extends Component {
           showPreviews={true}
           maxFileSize={50000000}
           onClose={this.handleClose}
+        />
+        <UploadProgressDialog
+          onClose={this.handleUploadCancel}
+          open={uploading}
+          progress={uploadProgress}
         />
       </div>
     );

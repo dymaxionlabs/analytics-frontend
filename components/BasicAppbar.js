72c8e13e83b8caa33ca6ede87ff0bac9c50c6f29
Damián Silvani <munshkr@gmail.com>
2019-03-25 10:22:42 -0300

    Add logo on BasicAppbar

--- components/BasicAppbar.js
+++ components/BasicAppbar.js
@@ -1,19 +1,25 @@
-import React from "react";
-import PropTypes from "prop-types";
-import withStyles from "@material-ui/core/styles/withStyles";
 import AppBar from "@material-ui/core/AppBar";
+import Avatar from "@material-ui/core/Avatar";
+import withStyles from "@material-ui/core/styles/withStyles";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import PropTypes from "prop-types";
+import React from "react";
 
 const styles = theme => ({
   appBar: {
     position: "relative"
+  },
+  logo: {
+    height: 25,
+    marginRight: theme.spacing.unit
   }
 });
 
 const BasicAppbar = withStyles(styles)(({ classes }) => (
   <AppBar position="absolute" color="default" className={classes.appBar}>
     <Toolbar>
+      <img src="/static/logo.png" className={classes.logo} />
       <Typography variant="h6" color="inherit" noWrap>
         Analytics
       </Typography>

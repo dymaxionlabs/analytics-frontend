52b99159ba2b0d0750133718b64d1f73ca01d73a
Damián Silvani <munshkr@gmail.com>
2019-04-05 08:57:11 -0300

    Create CliengoLoader component

---
+++ components/CliengoLoader.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default () => (
+  <script
+    dangerouslySetInnerHTML={{
+      __html: `(function() {
+        var ldk = document.createElement('script');
+        ldk.type = 'text/javascript';
+        ldk.async = true;
+        ldk.src = 'https://s.cliengo.com/weboptimizer/5ca60a88e4b0d077b85e4879/5ca60a88e4b0d077b85e487c.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(ldk, s);
+      })();`
+    }}
+  />
+);

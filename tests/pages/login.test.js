b619fcc0858ec356aebba107319598418c7c815a
Damián Silvani <munshkr@gmail.com>
2019-06-17 16:42:08 -0300

    Make i18n work on tests with a stub

--- tests/pages/login.test.js
+++ tests/pages/login.test.js
@@ -1,10 +1,12 @@
-import { shallow, mount } from "enzyme";
+// import i18n from "i18next";
+// import { I18nextProvider } from "react-i18next";
+import { mount } from "enzyme";
 import React from "react";
-import { I18nextProvider } from "react-i18next";
 import Login from "../../pages/login";
-// import i18n from "../i18n";
-import jest from "jest";
 
 describe("Login", () => {
-  it("", () => {});
+  it("have a title", () => {
+    const wrapper = mount(<Login query={{}} />);
+    expect(wrapper.find("h1").text()).toEqual("login.title");
+  });
 });

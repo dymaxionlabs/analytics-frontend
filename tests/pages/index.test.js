94da23072f96bde05bc1f372df2bff256859d3bd
Damián Silvani <munshkr@gmail.com>
2019-06-17 17:10:15 -0300

    Update Index test

--- tests/pages/index.test.js
+++ tests/pages/index.test.js
@@ -1,20 +1,16 @@
 import { mount } from "enzyme";
 import React from "react";
-import { I18nextProvider } from "react-i18next";
 import Index from "../../pages/index";
-// import i18n from "../i18n";
-import jest from "jest";
 
 describe("Index", () => {
   it("redirects to home when authenticated", () => {
-    // const wrapper = mount(
-    //   <I18nextProvider i18n={i18n}>
-    //     <Index />
-    //   </I18nextProvider>
-    // );
-    // const instance = wrapper.instance();
-    // const redirectTo = jest.spyOn(instance, "_redirectTo");
-    // expect(redirectTo).toHaveBeenCalledWith("/home");
-    // spy.mockRestore();
+    // FIXME Does not currently work because Index has multiple HoC wrappers...
+    // const redirectTo = jest
+    //   .spyOn(Index.prototype, "_redirectTo")
+    //   .mockImplementation(() => {});
+    //
+    // const wrapper = mount(<Index token="mytoken" query={{}} />);
+    // expect(redirectTo).toBeCalledWith("/home");
+    // redirectTo.mockClear();
   });
 });

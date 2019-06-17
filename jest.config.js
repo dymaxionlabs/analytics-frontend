d7b34312993e5be7ff45701abfef13208d87be6d
Damián Silvani <munshkr@gmail.com>
2019-06-17 18:28:14 -0300

    Ignore tests/e2e/ when running with jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   setupFiles: ["<rootDir>/tests/setup.js"],
-  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"]
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  modulePathIgnorePatterns: ["<rootDir>/tests/e2e/"]
 };

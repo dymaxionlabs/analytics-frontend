5d0a97a588ee22bd802d89294030ff1de5712ec8
Damián Silvani <munshkr@gmail.com>
2019-06-18 18:54:28 -0300

    Better login tests
    Use an ugly workaround for unwrapping HoC Page components.

--- tests/pages/login.test.js
+++ tests/pages/login.test.js
@@ -8,43 +8,50 @@ afterEach(() => {
 });
 
 describe("Login", () => {
+  const getWrapper = props => {
+    return mount(shallow(<Login {...props} />).get(0));
+  };
+
+  const assertInputChange = (wrapper, id, value) => {
+    const input = wrapper.find(`input#${id}`);
+    expect(input.props().value).toEqual("");
+
+    input.simulate("change", { target: { value: value } });
+    expect(wrapper.state(id)).toEqual(value);
+  };
+
+  const assertCheckboxChange = (wrapper, id) => {
+    const input = wrapper.find(`input#${id}`);
+    expect(input.props().checked).toBe(false);
+
+    input.simulate("change");
+    expect(wrapper.state(id)).toBe(true);
+  };
+
   it("has a title", () => {
-    const wrapper = mount(<Login query={{}} />);
+    // const wrapper = mount(<Login query={{}} />);
+    const wrapper = getWrapper({ query: {} });
     const title = wrapper.find("h1");
     expect(title.text()).toEqual("login.title");
   });
 
   it("has a username input sets state", () => {
-    const wrapper = shallow(<Login query={{}} />).dive();
-    const input = wrapper.find("#username").dive();
-    expect(input.props().value).toEqual("");
-
-    input.simulate("change", { target: { value: "john" } });
-    expect(wrapper.state("username")).toEqual("john");
+    const wrapper = getWrapper({ query: {} });
+    assertInputChange(wrapper, "username", "john");
   });
 
   it("has a password input sets state", () => {
-    const wrapper = shallow(<Login query={{}} />).dive();
-    const input = wrapper.find("#password").dive();
-    expect(input.props().value).toEqual("");
-
-    input.simulate("change", { target: { value: "hunter2" } });
-    expect(wrapper.state("password")).toEqual("hunter2");
+    const wrapper = getWrapper({ query: {} });
+    assertInputChange(wrapper, "password", "hunter2");
   });
 
-  // it("has a 'Remember Me' checkbox sets state", () => {
-  //   const wrapper = shallow(<Login query={{}} />).dive();
-  //   wrapper.update();
-  //   // FIXME: Enzyme fails to find checkbox!!!
-  //   const input = wrapper.find("#remember").dive();
-  //   expect(input.props().checked).toBe(false);
-
-  //   input.simulate("change");
-  //   expect(wrapper.state("remember")).toBe(true);
-  // });
+  it("has a 'Remember Me' checkbox sets state", () => {
+    const wrapper = getWrapper({ query: {} });
+    assertCheckboxChange(wrapper, "remember");
+  });
 
   it("has a form that makes a POST request to /auth/login with user credentials", () => {
-    const wrapper = shallow(<Login query={{ redirect: "/foo" }} />).shallow();
+    const wrapper = getWrapper({ query: { redirect: "/foo" } });
     const form = wrapper.find("form");
 
     // Mock login function
@@ -71,7 +78,7 @@ describe("Login", () => {
   });
 
   it("sets a longer expiration key when setting 'remember' state key", () => {
-    const wrapper = shallow(<Login query={{}} />).shallow();
+    const wrapper = getWrapper({ query: {} });
 
     const mockLogin = jest.fn();
     wrapper.instance()._login = mockLogin;
@@ -92,7 +99,7 @@ describe("Login", () => {
   });
 
   it("catches errors from API server and unlocks form", () => {
-    const wrapper = shallow(<Login query={{}} />).shallow();
+    const wrapper = getWrapper({ query: {} });
     wrapper.find("form").simulate("submit", { preventDefault: () => null });
 
     mockAxios.mockError();

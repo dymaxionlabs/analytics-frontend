e075f4138f27a7af3176caea9d0544ee2d188acf
Damián Silvani <munshkr@gmail.com>
2019-06-19 18:22:24 -0300

    Tests for Index page

--- tests/pages/index.test.js
+++ tests/pages/index.test.js
@@ -1,16 +1,31 @@
-import { mount } from "enzyme";
+import { mount, shallow } from "enzyme";
 import React from "react";
+import mockAxios from "jest-mock-axios";
 import Index from "../../pages/index";
 
 describe("Index", () => {
-  it("redirects to home when authenticated", () => {
-    // FIXME Does not currently work because Index has multiple HoC wrappers...
-    // const redirectTo = jest
-    //   .spyOn(Index.prototype, "_redirectTo")
-    //   .mockImplementation(() => {});
-    //
-    // const wrapper = mount(<Index token="mytoken" query={{}} />);
-    // expect(redirectTo).toBeCalledWith("/home");
-    // redirectTo.mockClear();
+  const getWrapper = props => {
+    return mount(shallow(shallow(<Index {...props} />).get(0)).get(0));
+  };
+
+  const originalLocation = window.location;
+
+  beforeAll(() => {
+    delete window.location;
+    window.location = { href: "#" };
+  });
+
+  afterAll(() => {
+    window.locaiton = originalLocation;
+  });
+
+  it("redirects to Home when authenticated", () => {
+    const wrapper = getWrapper({ token: "userToken", query: {} });
+    expect(window.location.href).toEqual("/home");
+  });
+
+  it("redirects to Quote when authenticated", () => {
+    const wrapper = getWrapper({ query: {} });
+    expect(window.location.href).toEqual("/quote");
   });
 });
